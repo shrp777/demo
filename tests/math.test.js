6ccf7ceb59454797f9c9646cb2c78976bd01d437
Alexandre Leroux <alex@sherpa.one>
2023-12-18 19:59:31 +0100

    divide

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "@jest/globals";
-import { add, multiply, square } from "../src/Math.js";
+import { add, divide, multiply, square } from "../src/Math.js";
 
 describe("Math", () => {
   test("add should be defined", () => {
@@ -11,6 +11,9 @@ describe("Math", () => {
   test("square should be defined", () => {
     expect(square).toBeDefined();
   });
+  test("divide should be defined", () => {
+    expect(divide).toBeDefined();
+  });
   test("add(1,1) should equal 2", () => {
     expect(add(1, 1)).toEqual(2);
   });
@@ -19,5 +22,8 @@ describe("Math", () => {
   });
   test("square(3) should equal 9", () => {
     expect(square(3)).toEqual(9);
+  });
+  test("divide(6,3) should equal 2", () => {
+    expect(divide(6, 3)).toEqual(2);
   });
 });
